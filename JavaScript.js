b9d6cf06200d550f1a73ae36dd4b64db7f7e39b0
Ekremteke <ekremteke.uk@gmail.com>
2022-06-10 19:38:38 +0100

    Adding to JavaScript training

--- JavaScript.js
+++ JavaScript.js
@@ -1,79 +1,60 @@
-const {
-    core: { test, expect, run },
-    prettify
-  } = window.jestLite;
-  
-  /* =================
-    FIND ELEMENTS
-    These are all the elements we will look for.
-     ===================
-  */
-  const getHeader = document.querySelectorAll("header"),
-    getH1 = document.querySelectorAll("h1"),
-    getSiteHeader = document.querySelectorAll(".c-site-header"),
-    getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-    getMain = document.querySelectorAll("main"),
-    getFooter = document.querySelectorAll("footer"),
-    getSiteFooter = document.querySelectorAll(".c-site-footer"),
-    getIFrame = document.querySelectorAll("iframe"),
-    getImage = document.querySelectorAll("img"),
-    getWords = document.body.innerText;
-  
-  /* =================
-     ASSERTIONS 
-     These are the things we check are true about your page.
-     Read and update your HTML to discover the requirements.
-     The tests will run every time you update your code.
-     ===================
-  */
-  test("There is at least one header element", () => {
-    expect(getHeader.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There is at least one h1", () => {
-    expect(getH1.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There is only one header element with the class c-site-header", () => {
-    expect(getSiteHeader.length).toBe(1);
-  });
-  test("There is a nav element with an aria-label of Main Site Links.", () => {
-    expect(getAria.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There is only one main element", () => {
-    expect(getMain.length).toBe(1);
-  });
-  test("There is at least one footer element", () => {
-    expect(getFooter.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There is only one footer element with the class c-site-footer", () => {
-    expect(getSiteFooter.length).toBe(1);
-  });
-  test("There is embedded video", () => {
-    expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There is at least one image", () => {
-    expect(getImage.length).toBeGreaterThanOrEqual(1);
-  });
-  test("There are at least 500 words on the page", () => {
-    expect(getWords.length).toBeGreaterThanOrEqual(500);
-  });
-  
-  const console = document.getElementById("tests");
-  prettify.toHTML(run(), console);
-  document.getElementById("middle").innerHTML = "Yusuf is Ekrem's son. He is 6 years old. He goes to reception class. He's middle name is Ertugrul.";
-  var links, name2;
-
-
+var links, name2;
 links = ['https://www.google.co.uk/', 'https://www.amazon.co.uk/', 'https://www.youtube.com/'];
 name2 = ['Google', 'Amazon', 'Youtube'];
 let element_linklist = document.getElementById('linklist');
 while (!!name2.length) {
-  if(--window.LoopTrap <= 0) throw "Infinite loop.";
-  let new_li = document.createElement('li');
-  let new_a = document.createElement('a');
-  new_a.innerText = name2.shift();
-  new_a.setAttribute("href", links.shift());
+ if(--window.LoopTrap <= 0) throw "Infinite loop.";
+ let new_li = document.createElement('li');
+ let new_a = document.createElement('a');
+ new_a.innerText = name2.shift();
+ new_a.setAttribute("href", links.shift());
 
-  new_li.appendChild(new_a);
+ new_li.appendChild(new_a);
 
-  element_linklist.appendChild(new_li);
+ element_linklist.appendChild(new_li);
 }
+
+var array;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+array = [];
+
+
+document.getElementById('add_button').addEventListener('click', (event) => {
+  array.push(getNumberOrString(document.getElementById('names').value));
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = getNumberOrString(document.getElementById('names').value);
+
+  element_list.appendChild(new_li);
+
+});
+
+document.getElementById('facilitator_button').addEventListener('click', (event) => {
+  let element_display = document.getElementById('display');
+  let new_li2 = document.createElement('li');
+  new_li2.innerText = randomMember(array);
+
+  element_display.appendChild(new_li2);
+
+});
