c3c0abc90a6aa449c1f68d0346fe3627f105cdaa
Ekremteke <ekremteke.uk@gmail.com>
2022-06-17 01:37:38 +0100

    second js

--- JavaScript.js
+++ JavaScript.js
@@ -1,20 +1,4 @@
-var links, name2;
-links = ['https://www.google.co.uk/', 'https://www.amazon.co.uk/', 'https://www.youtube.com/'];
-name2 = ['Google', 'Amazon', 'Youtube'];
-let element_linklist = document.getElementById('linklist');
-while (!!name2.length) {
- if(--window.LoopTrap <= 0) throw "Infinite loop.";
- let new_li = document.createElement('li');
- let new_a = document.createElement('a');
- new_a.innerText = name2.shift();
- new_a.setAttribute("href", links.shift());
-
- new_li.appendChild(new_a);
-
- element_linklist.appendChild(new_li);
-}
-
-var array;
+var commentList;
 
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
@@ -37,11 +21,11 @@ function randomMember(arr) {
 }
 
 
-array = [];
+commentList = [];
 
 
 document.getElementById('add_button').addEventListener('click', (event) => {
-  array.push(getNumberOrString(document.getElementById('names').value));
+  commentList.push(getNumberOrString(document.getElementById('names').value));
   let element_list = document.getElementById('list');
   let new_li = document.createElement('li');
   new_li.innerText = getNumberOrString(document.getElementById('names').value);
@@ -53,28 +37,13 @@ document.getElementById('add_button').addEventListener('click', (event) => {
 document.getElementById('facilitator_button').addEventListener('click', (event) => {
   let element_display = document.getElementById('display');
   let new_li2 = document.createElement('li');
-  new_li2.innerText = randomMember(array);
+  new_li2.innerText = randomMember(commentList);
 
   element_display.appendChild(new_li2);
 
 });
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-} 
 var arrayh1, storage;
 
 
@@ -94,3 +63,35 @@ document.getElementById('hobbies').addEventListener('click', (event) => {
   arrayh1.push(storage);
 
 });
+
+var links, name2;
+links = ['https://www.google.co.uk/', 'https://www.amazon.co.uk/', 'https://www.youtube.com/'];
+name2 = ['Google', 'Amazon', 'Youtube'];
+let element_linklist = document.getElementById('linklist');
+while (!!name2.length) {
+ if(--window.LoopTrap <= 0) throw "Infinite loop.";
+ let new_li = document.createElement('li');
+ let new_a = document.createElement('a');
+ new_a.innerText = name2.shift();
+ new_a.setAttribute("href", links.shift());
+
+ new_li.appendChild(new_a);
+
+ element_linklist.appendChild(new_li);
+}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+} 
